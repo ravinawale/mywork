69ebde77d6734ec33482f8c70315c102b8f913d4
ravi <parigha.nawale@gmail.com>
2023-01-09 19:54:18 +0530

    Spring boot on external tomcat

--- src/main/java/com/supershopee/product/ProductserviceApplication.java
+++ src/main/java/com/supershopee/product/ProductserviceApplication.java
@@ -2,12 +2,15 @@ package com.supershopee.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource(value = {"classpath:api_error_messages.properties"})
 @PropertySource(value = {"classpath:api_response_messages.properties"})
 public class ProductserviceApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProductserviceApplication.class, args);
 	}
